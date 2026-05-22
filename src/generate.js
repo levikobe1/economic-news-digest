685cd6ad9dad001f094a7e1c05475d04422fd94b
levikobe1 <levikobe1@gmail.com>
2026-05-22 13:13:27 +0300

    Update generate.js

--- src/generate.js
+++ src/generate.js
@@ -205,6 +205,16 @@ function generateHTML(digest, articles, feedStatus) {
 <html lang="he" dir="rtl">
 <head>
   <meta charset="utf-8" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2XPPQJJE7Q"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-2XPPQJJE7Q');
+  </script>
+  
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>הבוקר הכלכלי</title>
   <style>
